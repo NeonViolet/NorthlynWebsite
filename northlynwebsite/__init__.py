a47422ea1ff1cf8f10c51e3b17319e5a161db004
NeonViolet <gavinngo23@gmail.com>
2021-08-30 16:55:16 +0100

    added some error pages

--- northlynwebsite/__init__.py
+++ northlynwebsite/__init__.py
@@ -1,7 +1,20 @@
 # northlynwebsite/__init__.py
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+# Migrate(app.db)
+
 from northlynwebsite.core.views import core
+from northlynwebsite.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
